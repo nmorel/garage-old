9108a7efe9663a758459fa099f181a66403d20a2
Nicolas Morel <nmr.morel@gmail.com>
2016-01-30 12:38:21 +0100

    Update data

--- src/data/cars/index.js
+++ src/data/cars/index.js
@@ -1,5 +1,8 @@
-const cars = [
-  require('./1/data.json')
+module.exports = [
+  require('./1/data.json'),
+  require('./2/data.json'),
+  require('./3/data.json'),
+  require('./4/data.json'),
+  require('./5/data.json'),
+  require('./6/data.json')
 ];
-
-module.exports = cars;
